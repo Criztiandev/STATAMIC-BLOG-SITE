f5dbbe2555efabfa2683a8ac41b2db79d7f57f19
Criztian <criztiandev@gmail.com>
2025-10-03 13:14:35 +0800

    fix: update SSG copy config to only include existing directories

--- config/statamic/ssg.php
+++ config/statamic/ssg.php
@@ -59,13 +59,9 @@ return [
 
     'copy' => [
         public_path('build') => 'build',
-        public_path('css') => 'css',
-        public_path('js') => 'js',
-        public_path('images') => 'images',
         public_path('assets') => 'assets',
         public_path('favicon.ico') => 'favicon.ico',
         public_path('robots.txt') => 'robots.txt',
-        // Add other public files/directories as needed
     ],
 
     /*
